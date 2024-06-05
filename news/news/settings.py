6d69bb849ca0706ef2f9ec488d01673fb4fe330b
Asli Karadas <karadas.asli@gmx.at>
2024-06-05 07:50:34 +0000

    Add changes in settings

--- news/news/settings.py
+++ news/news/settings.py
@@ -10,13 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
 from django.contrib.messages import constants as messages 
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+import os
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
