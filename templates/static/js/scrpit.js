6886f34f0e329e9b4e8fcd10a01489cc141373ed
Asli Karadas <karadas.asli@gmx.at>
2024-06-05 04:54:55 +0000

    Add files to static

---
+++ templates/static/js/scrpit.js
@@ -0,0 +1,7 @@
+// set time for disapearing django-flash-message
+setTimeout(function () {
+    let messages = document.getElementById('flash-msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 1500);
+  
